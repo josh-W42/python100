fa471134bb129dc457a76fc1de69a6ddfb99c5d5
Josh W <wilson.jd022@gmail.com>
2023-11-23 06:51:18 -0800

    Light day32 complete

--- day32/main.py
+++ day32/main.py
@@ -21,7 +21,7 @@
 #         to_addrs="my_test_email@gmail.com",
 #         msg="Subject:Hello\n\nThis is the body of my email."
 #     )
-
+import random
 # Working with datatime lib
 #
 # import datetime as dt
@@ -37,4 +37,37 @@
 # print(date_of_birth)
 
 
-# Small
+# Small app idea by using both libs
+
+import smtplib
+import datetime as dt
+
+my_email = "my_email.@gmail.com"
+# To get a password to use:
+#   You could use your actual password (wouldn't recommend)
+#   Or you could use an app password (google recommends not creating these)
+password = "Yeah I'm not putting a password here"
+
+now = dt.datetime.now()
+weekday = now.weekday()
+if weekday == 1:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+
+    print(quote)
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(my_email, password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=my_email,
+            msg=f"Subject:Monday Motivation\n\n{quote}"
+        )
+
+# Ok so the idea is that every monday, you would get a motivational quote
+# but alone it would have to be opened everyday.
+# In order to run it everyday automatically, the course suggests using python anywhere
+# to run the code in the cloud and schedule when it should run.
+
+# I will not be doing this!
